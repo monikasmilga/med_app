242e8445cfe8a5c8c726277aaca3be17524e17c5
monikasmilga <tuuli705@gmail.com>
2017-09-07 09:51:58 +0300

    Edited Model extention

--- app/Models/MAUsersRolesConnections.php
+++ app/Models/MAUsersRolesConnections.php
@@ -1,8 +1,12 @@
 <?php namespace App\Models;
 
-class MAUsersRolesConnections extends CoreModel {
+use Illuminate\Database\Eloquent\Model;
+
+class MAUsersRolesConnections extends Model {
 
     protected $table = 'ma_users_roles_connections';
+
+    protected $updated_at = false;
 
     protected $fillable = ['user_id', 'role_id'];
 
