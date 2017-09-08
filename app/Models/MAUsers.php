c773412728004174b6d8297272f8c546bf5d04a9
monikasmilga <tuuli705@gmail.com>
2017-09-08 10:23:09 +0300

    Removed unnecessary fillables

--- app/Models/MAUsers.php
+++ app/Models/MAUsers.php
@@ -6,6 +6,6 @@ class MAUsers extends CoreModel {
 
     protected $table = 'ma_users';
 
-    protected $fillable = ['id', 'role_id', 'avatar_id', 'first_name', 'last_name', 'position', 'email', 'password', 'remember_token', 'show_in_front'];
+    protected $fillable = ['id', 'role_id', 'avatar_id', 'first_name', 'last_name', 'position', 'email', 'show_in_front'];
 
 }
