b59eb7cdf8232a834e91f3972bed7a9d2839d308
monikasmilga <tuuli705@gmail.com>
2017-09-11 10:06:36 +0300

    Created middleware, that checks for token

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,14 @@ use Illuminate\Http\Request;
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
+
+
 Route::post('users/signin', 'MAUsersController@signIn');
-Route::resource('/users', 'MAUsersController');
-Route::resource('/posts', 'MAPostsController');
+
+Route::group(['middleware' => 'auth.jwt'], function () {
+
+    Route::resource('/users', 'MAUsersController');
+    Route::resource('/posts', 'MAPostsController');
+
+});
+
